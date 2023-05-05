861ebb8634bb962fbb7523b3a8125e2696cd316e
Harry Ritchie <111759395+harry-ritchie@users.noreply.github.com>
2023-05-05 10:24:10 +0100

    MTDSA-16552: Simplifying writes

--- app/uk/gov/hmrc/mtdsatestsupportapi/models/request/deleteStatefulTestData/DeleteStatefulTestDataRawData.scala
+++ app/uk/gov/hmrc/mtdsatestsupportapi/models/request/deleteStatefulTestData/DeleteStatefulTestDataRawData.scala
@@ -16,7 +16,7 @@
 
 package uk.gov.hmrc.mtdsatestsupportapi.models.request.deleteStatefulTestData
 
-import play.api.libs.functional.syntax.toFunctionalBuilderOps
+import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
 case class DeleteStatefulTestDataRawData(vendorClientId: String, body: Option[JsValue])
@@ -25,5 +25,5 @@ object DeleteStatefulTestDataRawData {
   implicit val writes: Writes[DeleteStatefulTestDataRawData] = (
     (JsPath \ "vendorClientId").write[String] and
       (JsPath \ "body").writeNullable[JsValue]
-  )(delete => (delete.vendorClientId, delete.body))
+  )(unlift(DeleteStatefulTestDataRawData.unapply))
 }
