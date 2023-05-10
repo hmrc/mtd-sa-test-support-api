84225cdf4db261df115826350963c6af30b297d4
Harry Ritchie <111759395+harry-ritchie@users.noreply.github.com>
2023-05-10 12:08:03 +0100

    MTDSA-15615: Minor change to service spec

--- test/api/services/ServiceSpec.scala
+++ test/api/services/ServiceSpec.scala
@@ -27,6 +27,6 @@ trait ServiceSpec extends UnitSpec with Status with MimeTypes with HeaderNames {
 
   implicit val hc: HeaderCarrier = HeaderCarrier()
   implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.global
-  implicit val rc: RequestContext = RequestContext(hc, "X-123", EndpointLogContext("someController", "someEndpoint"))
+  implicit val rc: RequestContext = RequestContext(HeaderCarrier(), "X-123", EndpointLogContext("someController", "someEndpoint"))
 
 }
