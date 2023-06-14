6d4b6c27a3b324252c9febd4fa44429d68afa944
sean-flynn-HMRC <106095575+sean-flynn-HMRC@users.noreply.github.com>
2023-06-14 10:05:32 +0100

    MTDSA-17146 Bumped down to 2.13.8 for now to make sCoverage happy

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -25,20 +25,20 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-28" % bootstrap_28_version,
-    "org.typelevel"                %% "cats-core"                 % "2.8.0",
+    "org.typelevel"                %% "cats-core"                 % "2.9.0",
     "com.chuusai"                  %% "shapeless"                 % "2.4.0-M1",
     "com.neovisionaries"           % "nv-i18n"                    % "1.29",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.14.0"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.14.2"
   )
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
-    "org.scalatest"          %% "scalatest"              % "3.2.14"             % scope,
-    "com.vladsch.flexmark"   % "flexmark-all"            % "0.62.2"             % scope,
-    "org.scalatestplus"      %% "scalacheck-1-15"        % "3.2.10.0"           % scope,
+    "org.scalatest"          %% "scalatest"              % "3.2.15"             % scope,
+    "com.vladsch.flexmark"   % "flexmark-all"            % "0.64.0"             % scope,
+    "org.scalatestplus"      %% "scalacheck-1-15"        % "3.2.11.0"           % scope,
     "org.scalamock"          %% "scalamock"              % "5.2.0"              % scope,
     "com.typesafe.play"      %% "play-test"              % PlayVersion.current  % scope,
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrap_28_version % scope,
     "com.github.tomakehurst" % "wiremock-jre8"           % "2.35.0"             % scope,
-    "io.swagger.parser.v3"   % "swagger-parser-v3"       % "2.0.24"             % scope
+    "io.swagger.parser.v3"   % "swagger-parser-v3"       % "2.1.12"             % scope
   )
 }
